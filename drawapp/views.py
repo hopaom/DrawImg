b73157c5036be6ac5cf3f5cc583ba5ee5153c595
Ho Kim <hopao159@gmail.com>
2022-02-26 23:43:52 +0900

    수정 - 스타일 효과 변경, 스타일 다듬기

--- drawapp/views.py
+++ drawapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 
 def firstpage(request):
     return render(request, 'drawapp/firstpage.html')
+
+def aaa(request):
+    return render(request, 'drawapp/aaa.html')
