28e8b398d0553178e23ff87b857caf7d46ecf88d
Ho Kim <hopao159@gmail.com>
2022-02-28 16:31:45 +0900

    수정 - urls 경로 수정

--- DrawImg/urls.py
+++ DrawImg/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drawpage/', include('drawapp.urls')),
+    path('', include('drawapp.urls')),
 ]
